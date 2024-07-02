990e3d7ff5e98118ca217b7908e1bf54d695a0a1
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-02 14:52:15 +0200

    [UPD] created button event listener and a varialble for the name and one for the message

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,1 +1,9 @@
 const guestList = [ 'Odino', 'Caparezza', 'Cesare Augusto', 'Merlino', 'Biancaneve', 'Il Tutor', 'Ozzy'] 
+
+document.getElementById('check').addEventListener('click', function(){
+
+    let name = document.getElementById('name').value;
+
+    let result = document.getElementById('result'). value;
+    
+})
